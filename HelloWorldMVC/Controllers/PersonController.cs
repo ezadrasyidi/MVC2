e4224652ca0e346c763ad0e4ddb732ba8518aa9e
ezadrasyidi <33473592+ezadrasyidi@users.noreply.github.com>
2018-03-21 23:12:28 +0800

    aa

---
+++ HelloWorldMVC/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorldMVC.Models;
+
+namespace HelloWorldMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult addPerson(string name, int yob)
+        {
+            Person p = new Person(name, yob);
+            p.calculateAge();
+            return View(p);
+        }
+
+        [ActionName("Optional")]
+        public ActionResult addPerson()
+        {
+            Person p = new Person("Adib", 1996);
+            p.calculateAge();
+            return View(p);
+        }
+    }
+}
---
+++ HelloWorldMVC/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorldMVC.Models
+{
+    //Person/addPerson
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public int yob { get; set; }
+        public string category { get; set; }
+
+        public Person(string name, int yob)
+        {
+            this.name = name;
+            this.yob = yob;
+        }
+
+        public void calculateAge()
+        {
+            age = DateTime.Today.Year - yob;
+            category = "Adult";
+        }
+    }
+}
